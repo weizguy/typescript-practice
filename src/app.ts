145399a9d6bb78d374e32b75d7923760fa191013
Dave Weizenegger <dave.weizenegger@gmail.com>
2021-02-16 19:11:31 -0600

    section 4 complete

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,60 @@
-let userInput: unknown;
-let userName: string;
+// const userName = 'Dave';
+// // userName = 'Weizguy';
+// let age = 49;
 
-userInput = 5;
-userInput = 'Dave';
-if (typeof userInput === 'string') {
-  userName = userInput;
-}
+// age = 50;
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
-}
+// function add(a: number, b: number) {
+//   let result;
+//   result = a + b;
+//   return result;
+// }
 
-generateError('An error occurred!', 500);
+// if (age > 60) {
+//   let isOld = true;
+// }
+
+// console.log(isOld);
+
+// console.log(result);
+
+// const add = (a: number, b: number = 1) => a + b;
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if (button) {
+//   button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ['Programming', 'Gaming'];
+const activeHobbies = ['Working out'];
+
+activeHobbies.push(...hobbies);
+
+const person = {
+  firstName: 'Dave',
+  age: 49
+};
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
+
+const [hobby1, hobby2, ...remainingHobbies] = hobbies;
+
+console.log(hobbies, hobby1, hobby2);
+
+const { firstName: userName, age } = person;
+
+console.log(userName, age, person);
